7afb2064a71bdb7ddc21802c829baaf199af7ffc
jinxiu.qi <suiterchik@gmail.com>
2016-11-10 00:09:21 +0800

    .

--- chin.py
+++ chin.py
@@ -1,20 +1,43 @@
 #!/home/suit/dev/anaconda2/bin/python
-# coding=utf-8
+# -*- coding:utf-8 -*-
+__author__ = 'jinxiu.qi'
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 from core.scheduler import JobTracker
 from core.worker import TaskTracker
 from model import reset_db
-import sys
+from flask.ext.script import Manager, Shell, Server
+from webserver import create_app
+import ConfigParser
+
+
 if __name__ == "__main__":
     action = sys.argv[1]
-    if action == "webserver":
-        print "star webserver"
+    if action == "runserver":
+        cf = ConfigParser.ConfigParser()
+        cf.read('chin.ini')
+        host = cf.get('webserver', 'host')
+        port = cf.get('webserver', 'port')
+
+        app = create_app()
+        manager = Manager(app)
+        server = Server(host=host, port=port)
+        manager.add_command("runserver", server)
+        manager.run()
+
     elif action == "worker":
         worker = TaskTracker()
         worker.serve()
+
     elif action == "scheduler":
         scheduler = JobTracker()
         scheduler.serve()
+
     elif action == "resetdb":
         reset_db()
+
     else:
         print "不支持参数%s" % action
