bcb390883dc008760cb9d7f839248351bdb38e55
jinxiu.qi <suiterchik@gmail.com>
2016-11-12 19:34:29 +0800

    .

--- webserver/main/views.py
+++ webserver/main/views.py
@@ -1,11 +1,14 @@
 # coding=utf-8
 from . import admin
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from model import DBSession, Task
+import json
+
 
 @admin.route('/login')
 def login():
     return render_template('login.html')
+
 
 @admin.route('/')
 def home():
@@ -19,9 +22,22 @@ def list_task():
     return render_template('list_task.html')
 
 
-@admin.route('/new_task')
+@admin.route('/new_task', methods=['POST', 'GET'])
 def new_task():
-    return render_template('new_task.html')
+    if request.method == 'GET':
+        extend_id = request.args.get('extend_id')
+        return render_template('new_task.html')
+    else:
+        task_name = request.form.get('task_name')
+        command = request.form.get('command')
+        valid = request.form.get('valid')
+        priority = request.form.get('priority')
+        rerun = request.form.get('rerun')
+        rerun_time = request.form.get('rerun_times')
+        father_task = request.form.get('father_task')
+        machine_pool = request.form.get('machine_pool')
+        return '/' if request.form.get('has_next') == 'false' else 'new_task'
+
 
 
 @admin.route('/list_execute_log')
