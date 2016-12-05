76126f948448035fb59bc22afc7cf5474fb871bd
jinxiu.qi <suiterchik@gmail.com>
2016-12-06 01:18:26 +0800

    整理日志tab的js

--- webserver/static/scheduler/interaction.js
+++ webserver/static/scheduler/interaction.js
@@ -12,3 +12,20 @@ function kill_task(task_id, version) {
         }
     });
 }
+
+function fill_log_to_div(task_id, version, div_id) {
+    document.getElementById(div_id).innerText = '';
+    $.post('/get_log_detail', {'task_id': task_id, 'version': version}, function (result) {
+        document.getElementById(div_id).innerText = result;
+    });
+}
+
+function rerun(task_id, version) {
+    $.post('/rerun', {'task_id': task_id, 'version': version}, function () {
+        location.reload();
+    });
+}
+
+function list_instance_log(task_id) {
+    location.href = '/list_instance_log?task_id=' + task_id;
+}
