be2cca2d10f1774c03de479394efdb7a578c5335
jinxiu.qi <suiterchik@gmail.com>
2017-01-22 23:18:38 +0800

    添加机器

--- webserver/main/machine.py
+++ webserver/main/machine.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 from . import admin
-from flask import render_template, request
-from flask.ext.login import login_required
+from flask import render_template, request, redirect
+from model import Machine, Action, DBSession
+from flask.ext.login import login_required, current_user
+from datetime import datetime
 
 
 @admin.route('/list_machine')
@@ -16,10 +18,27 @@ def list_machine_status():
     return render_template('machine/list_status.html')
 
 
-@admin.route('/new_machine')
+@admin.route('/new_machine', methods=['GET', 'POST'])
 @login_required
 def new_machine():
-    return render_template('machine/new.html')
+    if request.method == 'GET':
+        return render_template('machine/new.html')
+    else:
+        print 'hello'
+        machine = Machine()
+        machine.name = request.form.get('name')
+        machine.ip = request.form.get('ip')
+        machine.mac = request.form.get('mac')
+        session = DBSession()
+        session.add(machine)
+        session.commit()
+
+        action = Action(user_name=current_user.name, content='新建机器 {}'.format(machine.name), create_time=datetime.now())
+        session.add(action)
+        session.commit()
+
+        session.close()
+        return redirect('/list_machine')
 
 
 @admin.route('/modify_machine', methods=['POST', 'GET'])
