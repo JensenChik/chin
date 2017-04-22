beae0d2fad0ab1214bb5bd6aff1332d736d42a84
jinxiu.qi <suiterchik@gmail.com>
2017-04-22 22:35:09 +0800

    添加启动页

---
+++ app/app/src/main/java/space/conj/chin/LaunchActivity.java
@@ -0,0 +1,33 @@
+package space.conj.chin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by hit-s on 2017/4/22.
+ */
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar bar = getSupportActionBar();
+        if (bar != null) {
+            bar.hide();
+        }
+        setContentView(R.layout.launch);
+        Log.i("Home", "进入主页面");
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(LaunchActivity.this, TaskActivity.class));
+                finish();
+            }
+        }, 500);
+    }
+
+}
