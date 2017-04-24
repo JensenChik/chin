80312cc648f330f96c161a06fdd877e3152e51c6
jinxiu.qi <suiterchik@gmail.com>
2017-04-24 23:44:36 +0800

    主页根据是否存在cookie决定跳转到任务页or登陆页

--- app/app/src/main/java/space/conj/chin/LaunchActivity.java
+++ app/app/src/main/java/space/conj/chin/LaunchActivity.java
@@ -8,10 +8,20 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.squareup.okhttp.OkHttpClient;
+
+import java.net.CookieManager;
+import java.net.HttpCookie;
+import java.util.List;
+
+import space.conj.chin.tools.RequestClient;
+
 /**
  * Created by hit-s on 2017/4/22.
  */
 public class LaunchActivity extends AppCompatActivity {
+
+    private OkHttpClient client;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,10 +31,17 @@ public class LaunchActivity extends AppCompatActivity {
             bar.hide();
         }
         setContentView(R.layout.launch);
+        client = RequestClient.getInstance();
         Log.i("Home", "进入主页面");
         new Handler().postDelayed(new Runnable() {
             public void run() {
-                startActivity(new Intent(LaunchActivity.this, LoginActivity.class));
+                if (RequestClient.hasCookieOf("chin.nazgrim.com")) {
+                    Log.i("LaunchActivity", "cookie已存在，直接跳转任务页");
+                    startActivity(new Intent(LaunchActivity.this, ListTaskActivity.class));
+                } else {
+                    Log.i("LaunchActivity", "cookie未存在，跳转登陆页");
+                    startActivity(new Intent(LaunchActivity.this, LoginActivity.class));
+                }
                 finish();
             }
         }, 500);
