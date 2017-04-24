d2d6fd72263a24ca3c666ece45af793727b3dbf2
jinxiu.qi <suiterchik@gmail.com>
2017-04-24 23:42:39 +0800

    添加判断是否存在domain的cookie静态方法

--- app/app/src/main/java/space/conj/chin/tools/RequestClient.java
+++ app/app/src/main/java/space/conj/chin/tools/RequestClient.java
@@ -3,6 +3,8 @@ package space.conj.chin.tools;
 import com.squareup.okhttp.OkHttpClient;
 
 import java.net.CookieManager;
+import java.net.HttpCookie;
+import java.util.List;
 
 /**
  * Created by hit-s on 2017/4/24.
@@ -16,6 +18,18 @@ public class RequestClient {
 
     public static OkHttpClient getInstance() {
         return client;
+    }
+
+    public static boolean hasCookieOf(String domain){
+        boolean hasCookie = false;
+        List<HttpCookie> cookies = ((CookieManager) client.getCookieHandler()).getCookieStore().getCookies();
+        for (HttpCookie cookie : cookies) {
+            if(cookie.getDomain().equals(domain)) {
+                hasCookie = true;
+                break;
+            }
+        }
+        return hasCookie;
     }
 
     ;
