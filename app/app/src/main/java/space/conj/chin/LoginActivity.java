b012da01b81305102b45ab549a76761ef7cf163c
jinxiu.qi <suiterchik@gmail.com>
2017-04-25 23:02:42 +0800

    自动登陆

--- app/app/src/main/java/space/conj/chin/LoginActivity.java
+++ app/app/src/main/java/space/conj/chin/LoginActivity.java
@@ -1,14 +1,18 @@
 package space.conj.chin;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Looper;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -27,9 +31,13 @@ import space.conj.chin.tools.RequestClient;
  */
 public class LoginActivity extends AppCompatActivity {
 
-    Button login;
-    EditText userName;
-    EditText password;
+    private Button login;
+    private EditText userName;
+    private EditText password;
+    private CheckBox rememberPassword;
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private boolean isRemember;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,10 +50,25 @@ public class LoginActivity extends AppCompatActivity {
         login = (Button) findViewById(R.id.login);
         userName = (EditText) findViewById(R.id.user_name);
         password = (EditText) findViewById(R.id.password);
+        rememberPassword = (CheckBox) findViewById(R.id.remember_password);
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = pref.edit();
+        isRemember = pref.getBoolean("remember_password", false);
+
+
+        if (isRemember) {
+            userName.setText(pref.getString("user_name", ""));
+            password.setText(pref.getString("password", ""));
+            rememberPassword.setChecked(true);
+        }
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+                progressDialog.setTitle("正在登陆中");
+                progressDialog.setMessage("耐心等待...");
+                progressDialog.show();
 
                 OkHttpClient client = RequestClient.getInstance();
 
@@ -65,19 +88,33 @@ public class LoginActivity extends AppCompatActivity {
                     public void onResponse(Response response) throws IOException {
                         if (response.headers().names().contains("Set-Cookie")) {
                             Log.i("LOGIN COOKIE", response.header("Set-Cookie"));
+                            if (rememberPassword.isChecked()) {
+                                editor.putBoolean("remember_password", true);
+                                editor.putString("user_name", userName.getText().toString());
+                                editor.putString("password", password.getText().toString());
+                            } else {
+                                editor.clear();
+                            }
+                            editor.apply();
                             startActivity(new Intent(LoginActivity.this, ListTaskActivity.class));
+                            progressDialog.dismiss();
                             finish();
                         } else {
                             Looper.prepare();
+                            editor.clear();
+                            editor.apply();
                             Toast.makeText(LoginActivity.this, "登陆账号密码有误", Toast.LENGTH_SHORT).show();
                             Looper.loop();
                         }
-
                     }
                 });
-
+                progressDialog.hide();
             }
         });
+
+        if (isRemember) {
+            login.performClick();
+        }
 
 
     }
