1bfcbda5c77ff439adc3badc6641d0d7ab48061e
jinxiu.qi <suiterchik@gmail.com>
2017-05-10 01:19:18 +0800

    支持含参方法调用

--- app/app/src/main/java/space/conj/chin/tools/NewThread.java
+++ app/app/src/main/java/space/conj/chin/tools/NewThread.java
@@ -27,4 +27,32 @@ public class NewThread {
 
 
     }
+
+    public static void run(final Object object, String methodName, final Object[] args) {
+        try {
+            Class<?>[] argsClassType = new Class[args.length];
+            for (int i = 0; i < args.length; i++) {
+                argsClassType[i] = args[i].getClass();
+            }
+
+            final Method method = object.getClass().getDeclaredMethod(methodName, argsClassType);
+            method.setAccessible(true);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        method.invoke(object, args);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 }
