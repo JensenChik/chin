33bd8b5f3c3efc0739f4c75c8d2242eecc5eeb37
jinxiu.qi <suiterchik@gmail.com>
2017-10-15 22:13:13 +0800

    update

--- chin.go
+++ chin.go
@@ -6,22 +6,17 @@ import (
     "./core/scheduler"
     "./core/worker"
     "./server"
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/mysql"
+    "./database"
 )
 
-func init_db() {
-    fmt.Println("链接mysql")
-    db, _ := gorm.Open("mysql", "user:password@tcp(host:port)/dbname?charset=utf8&parseTime=True&loc=Local")
-    fmt.Println(db.HasTable("user"))
-    defer db.Close()
-}
+
 
 func main() {
     if len(os.Args) != 2 {
         fmt.Println("必须附带启动参数：scheduler / worker / webserver / init")
         os.Exit(1)
     }
+
     switch os.Args[1] {
     case "scheduler":
         fmt.Println("启动 scheduler")
@@ -33,8 +28,7 @@ func main() {
         fmt.Println("启动 api 服务")
         server.Serve()
     case "init_db":
-        fmt.Println("初始化数据库")
-        init_db()
+        database.Init()
     default:
         fmt.Println("不支持启动命令: " + os.Args[1])
     }
