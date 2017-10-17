60dd42d32aded5fedb8a03b707d00dbc8fec30e1
jinxiu.qi <suiterchik@gmail.com>
2017-10-18 01:30:14 +0800

    添加dumpToMysql方法以及实现existsUser函数

--- database/user.go
+++ database/user.go
@@ -1,6 +1,9 @@
 package database
 
-import "github.com/jinzhu/gorm"
+import (
+    "github.com/jinzhu/gorm"
+    "log"
+)
 
 type User struct {
     gorm.Model
@@ -9,6 +12,26 @@ type User struct {
     Email    string
 }
 
+func (user User) DumpToMySQL() bool {
+    db, connectError := ConnectDatabase()
+    defer db.Close()
+    if connectError != nil {
+        return false
+    }
+    create := db.Create(&user)
+    if create.Error != nil {
+        log.Print("[Error] 插入user记录失败, ", create.Error)
+        return false
+    } else {
+        return true
+    }
+}
+
 func ExistsUser(userName string, password string) bool {
-    return false
+    md5Passwd := toMD5(password)
+    db, _ := ConnectDatabase()
+    defer db.Close()
+    user := User{}
+    db.Where("user_name =?", userName).First(&user)
+    return user.Password == md5Passwd
 }
