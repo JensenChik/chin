94a82dce920baf97c25c0042fc9ededab314878b
jinxiu.qi <suiterchik@gmail.com>
2017-10-18 00:01:29 +0800

    添加字符串压缩

--- database/tools.go
+++ database/tools.go
@@ -5,12 +5,31 @@ import (
     "crypto/md5"
     "encoding/hex"
     "math/rand"
+    "bytes"
+    "compress/zlib"
+    "io"
 )
 
 func toMD5(raw string) string {
     ctx := md5.New()
     ctx.Write([]byte(raw))
     return hex.EncodeToString(ctx.Sum(nil))
+}
+
+func zip(raw string) string {
+    var buffer bytes.Buffer
+    writer := zlib.NewWriter(&buffer)
+    writer.Write([]byte(raw))
+    writer.Close()
+    return buffer.String()
+}
+
+func unzip(raw string) string {
+    reader := bytes.NewReader([]byte(raw))
+    var buffer bytes.Buffer
+    r, _ := zlib.NewReader(reader)
+    io.Copy(&buffer, r)
+    return buffer.String()
 }
 
 func randomString(size int) string {
