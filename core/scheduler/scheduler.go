c0eb6552be6e4fec299c27e54b09eabb393c96a0
jinxiu.qi <suiterchik@gmail.com>
2017-10-27 22:02:33 +0800

    .

--- core/scheduler/scheduler.go
+++ core/scheduler/scheduler.go
@@ -2,7 +2,39 @@ package scheduler
 
 import "fmt"
 
-func Serve(){
+func makePooling() {
+
+}
+
+func makeWaiting() {
+
+}
+
+func makeRunning() {
+
+}
+
+func makeKilling() {
+
+}
+
+func makeSucceed() {
+
+}
+
+func makeFailed() {
+
+}
+
+func makeNotified() {
+
+}
+
+func makeFinish() {
+
+}
+
+func Serve() {
     fmt.Println("调度器开始工作")
 
 }
