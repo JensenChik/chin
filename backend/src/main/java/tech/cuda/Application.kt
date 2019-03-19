6bf778d69fd3ffad33c7ec21c3be68f98a689891
jinxiu.qi <suiterchik@gmail.com>
2019-03-20 00:01:19 +0800

    添加路由拦截

--- backend/src/main/java/tech/cuda/Application.kt
+++ backend/src/main/java/tech/cuda/Application.kt
@@ -2,69 +2,32 @@ package tech.cuda
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
-import tech.cuda.models.rebuildTables
-import java.util.*
-import java.util.regex.Pattern
 import javax.servlet.*
 import javax.servlet.http.HttpServletRequest
 
 /**
  * Created by Jensen on 18-6-15.
  */
+
+
 @SpringBootApplication
 open class Application : WebMvcConfigurerAdapter(), Filter {
 
-    private val REWRITE_TO = "rewriteUrl" //需要rewrite到的目的地址
-    private val REWRITE_PATTERNS = "urlPatterns" //拦截的url,url通配符之前用英文分号隔开
-    private var urlPatterns: Set<String>? = null//配置url通配符
-    private var rewriteTo: String? = null
-
-
-    override fun init(cfg: FilterConfig) {
-        val exceptUrl = cfg.getInitParameter(REWRITE_PATTERNS) ?: ""
-        rewriteTo = cfg.getInitParameter(REWRITE_TO)
-        urlPatterns = if (exceptUrl.isNotEmpty()) {
-            Collections.unmodifiableSet(HashSet(Arrays.asList(
-                    *exceptUrl.split(";".toRegex()).dropLastWhile({
-                        it.isEmpty()
-                    }).toTypedArray()
-            )))
-        } else {
-            emptySet()
-        }
-    }
+    override fun init(cfg: FilterConfig) {}
 
     override fun doFilter(req: ServletRequest, resp: ServletResponse, chain: FilterChain) {
-        fun isMatches(patterns: Set<String>?, url: String): Boolean {
-            if (null == patterns) {
-                return false
-            }
-            for (str in patterns) {
-                if (str.endsWith("/*")) {
-                    val name = str.substring(0, str.length - 2)
-                    if (url.contains(name)) {
-                        return true
-                    }
-                } else {
-                    val pattern = Pattern.compile(str)
-                    if (pattern.matcher(url).matches()) {
-                        return true
-                    }
-                }
-            }
-            return false
-        }
-
-        val request = req as HttpServletRequest
-        val servletPath = request.servletPath
-        val context = request.contextPath
-        //匹配的路径重写
-        if (isMatches(urlPatterns, servletPath)) {
-            req.getRequestDispatcher("$context/$rewriteTo").forward(req, resp)
-        } else {
+        req as HttpServletRequest
+        val isBackendRouter = req.servletPath.startsWith("/dist/")
+                || req.servletPath in listOf("/index.html", "/favicon.ico")
+                || req.servletPath.startsWith("/api/")
+        if (isBackendRouter) {
             chain.doFilter(req, resp)
+        } else {
+            req.getRequestDispatcher("${req.contextPath}/index.html").forward(req, resp)
         }
     }
 
@@ -75,6 +38,18 @@ open class Application : WebMvcConfigurerAdapter(), Filter {
         registry!!.addResourceHandler("/static/**").addResourceLocations("classpath:/static/")
         super.addResourceHandlers(registry)
     }
+
+
+    @Bean
+    open fun registerFilter(): FilterRegistrationBean {
+        val registration = FilterRegistrationBean()
+        registration.setName("RewriteFilter")
+        registration.filter = Application()
+        registration.addUrlPatterns("/*")
+        registration.order = 1
+        return registration
+    }
+
 }
 
 fun main(args: Array<String>) {
